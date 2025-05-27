43fe3b38294e99077b9d5702a2919d5a28bbf96b
vatsamit <128714694+vatsamit@users.noreply.github.com>
2025-05-27 19:58:29 +0530

    path added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
+const auth = require('../middleware/auth');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
