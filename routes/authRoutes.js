2893bbc2a923535d6911b46b22864c78e4b44cb2
vatsamit <128714694+vatsamit@users.noreply.github.com>
2025-05-27 20:02:41 +0530

    minor change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,6 @@ const auth = require('../middleware/auth');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
-router.put('/edit-profile', auth,editProfile);
+router.put('/edit-profile', auth, authController.editProfile);
 
 module.exports = router;
